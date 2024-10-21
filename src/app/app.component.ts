341a010fc152e6d815f097ac0608eaa67515f4ae
Landry Dupont <landrydupont790@gmail.com>
2024-10-21 10:15:31 +0200

    create component playing-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from "./component/playing-card/playing-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [PlayingCardComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.compenent.css"
 })
 export class AppComponent {
-  title = 'playing-cards';
+
 }
