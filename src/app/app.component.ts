e2c5d4b93ab8264e4319078c68d084b1f1396815
Landry Dupont <landrydupont790@gmail.com>
2024-10-22 15:04:41 +0200

    create for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,27 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, model, signal } from '@angular/core';
 import { PlayingCardComponent } from "./components/playing-card/playing-card.component";
 import { Monster } from './models/monster.model';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { MonsterType } from './utils/monster.utils';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayingCardComponent, SearchBarComponent],
+  imports: [PlayingCardComponent, SearchBarComponent, CommonModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.compenent.css"
 })
 export class AppComponent {
 
   monsters! : Monster[];
-  count: number = 0;
-  search = "";
+  search = model("");
 
-  selectedMonsterIndex = signal(1);
-  selectedMonster = computed(() => {
-    return this.monsters[this.selectedMonsterIndex()];
+  filteredMonsters = computed(() => {
+    return this.monsters.filter(monster => monster.name.includes(this.search()))
   })
 
   constructor() {
-
-    effect(() => {
-      console.log(this.selectedMonster())
-    })
 
     this.monsters = []
 
@@ -43,13 +38,23 @@ export class AppComponent {
     monster2.hp = 60;
     monster2.figureCaption = "N°003 Car";
     this.monsters.push(monster2);
+
+    const monster3 = new Monster();
+    monster3.name = "Rab";
+    monster3.image = "assets/img/monster-rabbit.png"
+    monster3.type = MonsterType.PLANT;
+    monster3.hp = 60;
+    monster3.figureCaption = "N°004 Rab";
+    this.monsters.push(monster3);
+
+    const monster4 = new Monster();
+    monster4.name = "Deer";
+    monster4.image = "assets/img/monster-deer.png"
+    monster4.type = MonsterType.FIRE;
+    monster4.hp = 60;
+    monster4.figureCaption = "N°005 Deer";
+    this.monsters.push(monster4);
   }
 
-  increaseCount() {
-    this.count++;
-  }
 
-  toggleMonster() {
-    this.selectedMonsterIndex.set((this.selectedMonsterIndex() + 1) % this.monsters.length)
-  }
 }
